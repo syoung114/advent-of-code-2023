2c1ba70e5ffd22006e1ce50104eca66f9070efbe
Steven Young <steven.young114@gmail.com>
2023-12-01 14:20:52 +1100

    add day1 file

--- app/main.hs
+++ app/main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import System.Environment (getArgs)
+
+import Day1
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+     args <- getArgs
+     case args of
+         [] -> putStrLn "no args provided"
+         ( program : _ ) -> do
+             output <- case program of
+                 "day1" -> day1
+                 _ -> return ["invalid choice: " ++ program]
+             putStrLn $ unlines output
+         --_ -> putStrLn "foobar"
