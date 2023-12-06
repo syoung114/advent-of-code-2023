0a770825675b091664770267c1b5a7cc6b6d99ad
Steven Young <steven.young114@gmail.com>
2023-12-06 21:41:47 +1100

    refactor day4 for formatting

--- src/Day4.hs
+++ src/Day4.hs
@@ -18,41 +18,40 @@ pow2Clamp n
   | n <= 0 = 0 --prevent rausing to negative power. don't want it in this problem.
   | otherwise = 2 ^ (n - 1)
 
-numMatches :: String -> Int
-numMatches x =
-  countMatching
-    $ both (filter isNumeric) -- Clean up things the splitting couldn't do. Using tuples to hide using the same function twice. 
-    $ tuple (
-        map (splitOn " ") -- [["1", "2", "3"], ["4", ...]]
-          $ splitOn " | " x -- "1 2 3 | 4 5 6" -> ["1 2 3", "4 5 6"]
-      )
-    where
-      tuple :: [a] -> (a , a)
-      tuple [x, y] = (x, y)
-
-      isNumeric :: String -> Bool
-      isNumeric xs = all isDigit xs && xs /= ""
-
-removeGameCount :: [String] -> [String]
-removeGameCount xs = map (head . tail . splitOn ": ") xs -- during computation we want [[]] -> [] so head does that
-
-fn :: [String] -> Int
-fn xs = fn' (removeGameCount xs) 0
+reformat :: [String] -> [([Int] , [Int])]
+reformat xs =
+  map
+  (
+    \x ->
+      both (map strToInt . filter isNumeric) -- Clean up things the splitting couldn't do. 
+      $ tuple -- Wrap inner map in tuple to avoid explicitly using the same function twice.
+        (
+          map
+          (splitOn " ")
+          $ splitOn " | " -- "1 2 3 | 4 5 6" -> [["1", "2", "3"], ["4", "5", "6"]]
+            $ (head . tail . splitOn ": ") x -- during computation we want [[]] -> [] so head does that
+        )
+  )
+  xs
   where
+    tuple :: [a] -> (a , a)
+    tuple [x, y] = (x, y)
 
-    fn' :: [String] -> Int -> Int
-    fn' [] acc = acc
-    fn' (x:xs) acc =
-      let 
-        matches = numMatches x
-      in
-        fn' xs (acc + pow2Clamp matches)
+    isNumeric :: String -> Bool
+    isNumeric xs = all isDigit xs && xs /= ""
+
+    strToInt :: String -> Int
+    strToInt xs = read xs :: Int
+
+fn :: [([Int] , [Int])] -> Int
+fn xs = foldl (\acc x -> acc + pow2Clamp (countMatching x)) 0 xs
 
 day4a :: IO [String]
 day4a = do
-  input <- readFile "../input/day4.txt"
-  let flines = lines input
-  return [show $ fn flines]
+  content <- readFile "../input/day4.txt"
+  let flines = lines content
+      input = reformat flines
+  return [show $ fn input]
 
 -- part 2 ---------------------------------------------------
 
@@ -77,16 +76,13 @@ cardsWinCards pointTable length l2 = cardsWinCards' pointTable 0 length (replica
             cardsWinCards' pointTable (i + 1) length instances
 
 pointsPerCard :: [String] -> [Int]
-pointsPerCard xs = map numMatches (removeGameCount xs)
+pointsPerCard xs = map countMatching (reformat xs)
 
-fn2 :: [String] -> Int
-fn2 xs = sum (cardsWinCards allPoints numCards numCards)
-  where
-    allPoints = pointsPerCard xs
-    numCards = length allPoints
+fn2 :: [Int] -> Int
+fn2 xs = sum (cardsWinCards xs (length xs) (length xs))
 
 day4b :: IO [String]
 day4b = do
-  input <- readFile "../input/day4.txt"
-  let flines = lines input
-  return [show $ fn2 flines]
+  content <- readFile "../input/day4.txt"
+  let flines = lines content
+  return [show $ fn2 $ pointsPerCard flines]
