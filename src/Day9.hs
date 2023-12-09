dbe260a938ef131b9b683ab8c0a567adbf7969fe
Steven Young <steven.young114@gmail.com>
2023-12-09 21:00:16 +1100

    Day9a: correct sample input

--- src/Day9.hs
+++ src/Day9.hs
@@ -3,27 +3,43 @@ module Day9 where
 import Statistics.LinearRegression
 import qualified Data.Vector.Unboxed as U
 
-guessNext :: [Int] -> Int
-guessNext xs = 
-  let
+import Data.Tuple.Extra (both)
+
+grad :: [Int] -> (Int , Int)
+grad xs = both round $ mxb 
+  where
     uxs = U.fromList [1.. fromIntegral $ length xs]
     uys = U.fromList $ map fromIntegral xs
-    (alpha, beta) = linearRegression uxs uys
-  in
-    round $ (fromIntegral $ length xs + 1) * (alpha * fromIntegral (length xs) + beta)
+    mxb = linearRegression uxs uys
+
+splitDifferences :: [Int] -> [Int]
+splitDifferences xs = zipWith (-) (tail xs) xs
+
+guessNext :: [Int] -> Int
+guessNext xs = guessNext' xs 0
+  where
+    guessNext' :: [Int] -> Int -> Int
+    guessNext' xs acc = 
+      let
+        (beta, alpha) = grad xs
+      in
+        if beta == 0 then
+          alpha * (length xs + 1) + acc
+        else
+          guessNext' (splitDifferences xs) (acc + (last xs))
 
 sumNexts :: [[Int]] -> Int
 sumNexts xs = foldl (\acc x -> acc + guessNext x) 0 xs
 
--- reformat :: [String] -> [[Int]]
--- reformat xs = map (\x -> map strToInt $ words x) xs
---   where
---     strToInt :: String -> Int
---     strToInt xs = read xs :: Int
+reformat :: [String] -> [[Int]]
+reformat xs = map (\x -> map strToInt $ words x) xs
+  where
+    strToInt :: String -> Int
+    strToInt xs = read xs :: Int
 
 day9a :: IO [String]
 day9a = do
   contents <- readFile "../input/day9.txt"
   let flines = lines contents
-      --input = reformat flines
-  return [show $ sumNexts [[1,3,6,10,15,21]]]
+      input = reformat flines
+  return [show $ sumNexts input]
