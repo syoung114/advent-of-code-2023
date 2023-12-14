2e72081cbc0f4cfde429eebbe579016e2932a2f1
Steven Young <steven.young114@gmail.com>
2023-12-14 15:59:20 +1100

    Day 13a progress

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,18 +1,51 @@
 module Day13 where
 
 import Data.List
+import Data.Array
 
-hScan :: [String] -> Int
-hScan xs = 0
+vScan :: Array (Int , Int) Char -> Int
+vScan xs = vScan' xs 0 (fst $ snd $ bounds xs)
+  where
+    vScan' :: Array (Int , Int) Char -> Int -> Int -> Int
+    vScan' xs i end
+      | i >= (fst $ snd $ bounds xs) = 0 -- reached end without finding a mirror
+      | (rowAt i xs) /= (rowAt end xs) = vScan' xs (i+1) end -- no mirror but keep going
+      | otherwise = round $ (fromIntegral (i + end)) / 2.0 -- calculate the relative midpoint
 
-vScan :: [String] -> Int
-vScan xs = 0
+hScan :: Array (Int , Int) Char -> Int
+hScan xs = vScan $ rot90right xs
 
-fn :: [[String]] -> Int
+fn :: [Array (Int , Int) Char] -> Int
 fn xs = foldl (\acc x -> acc + hScan x + 100 * vScan x) 0 xs
 
-reformat :: [String] -> [[String]]
-reformat xs = map (\x -> filter (/= "") x) $ groupBy (\x y -> y /= "") xs
+-- boilerplate begins here
+rowAt :: Int -> Array (Int , Int) Char -> String
+rowAt j xs = [xs ! (j,i) | i <- [0..(fst $ snd $ bounds xs)]]
+
+rot90right :: Array (Int , Int) a -> Array (Int , Int) a
+rot90right arr = ixmap (bounds arr) (\(j,i) -> (innerMax - i, j)) arr
+  where
+    innerMax = fst $ snd $ bounds arr
+
+reformat :: [String] -> [Array (Int , Int) Char]
+reformat xs =
+  map
+  (
+    \x ->
+      let
+        lenInner = (length (head x) - 1)
+        lenOuter = length x - 1
+        cxx = concat x
+      in
+        listArray ((0,0),(lenInner,lenOuter)) cxx
+  )
+  $ map
+    (
+      \x -> filter (/= "") x
+    )
+    $ groupBy
+      (\x y -> y /= "")
+      xs
 
 day13a :: IO [String]
 day13a = do
